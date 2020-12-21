7704e917d2532c731fea6546eb2187792ba9c2fe
Vinayak <vinayakdeepu@gmail.com>
2020-12-21 14:28:47 +0530

    Add firebase auth package

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyB3LWlKBG0TTWwMDGdBT_3wa1gBzx5T86s",
+  authDomain: "chorder-cf7cc.firebaseapp.com",
+  projectId: "chorder-cf7cc",
+  storageBucket: "chorder-cf7cc.appspot.com",
+  messagingSenderId: "453062768263",
+  appId: "1:453062768263:web:27b476ec0d2ed4bca62a22"
+};
